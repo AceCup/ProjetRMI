e1e3dff220f956496a142199c16447e5a4ad2fe8
Arioli Vincent <55313077+Ornael@users.noreply.github.com>
2020-02-11 17:18:07 +0100

    Add files via upload

--- ProjetJeux/src/jeu_de_pendu_impl.java
+++ ProjetJeux/src/jeu_de_pendu_impl.java
@@ -1,11 +1,25 @@
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
 
-public class jeu_de_pendu_impl extends UnicastRemoteObject implements jeu_de_pendu_interf{
+public class jeu_de_pendu_impl extends UnicastRemoteObject implements jeu_de_pendu_interf{	
 	public jeu_de_pendu_impl() throws RemoteException{
 		
 	}
 	public String commencer() {
+		ArrayList<String> ListeMots = new ArrayList<String>(); 
+		ListeMots.add("rouge");
+		ListeMots.add("jaune");
+		ListeMots.add("bleu");
+		ListeMots.add("violet");
+		ListeMots.add("noir");
+		int rand = (int)(Math.random() * ListeMots.size());
+		String Mot = ListeMots.get(rand);
+		char[] montablo = new char[Mot.length()];
+		for(int i = 0 ; i < Mot.length(); i++) {
+			montablo[i] = '_';
+			System.out.println(montablo[i]);
+		}
 		return "tapez 555 pour commencer Henshin";
 	}
 }
